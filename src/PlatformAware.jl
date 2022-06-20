192b73ebffb617121f187991c7fbd073f995fc26
Francisco Heron de Carvalho Junior <heron@dc.ufc.br>
2022-06-20 11:41:14 -0400

    initial commit

--- src/PlatformAware.jl
+++ src/PlatformAware.jl
@@ -1,5 +1,10 @@
+# ------------------------------------------------------------------
+# Licensed under the MIT License. See LICENCE in the project root.
+# ------------------------------------------------------------------
+
 module PlatformAware
 
-# Write your package code here.
+include("quantifiers/quantifiers.jl")
+include("platformtypes/platformtypes.jl")
 
 end
