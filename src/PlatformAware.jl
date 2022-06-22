1335e796f7c34436961bafe5925d5e9c4913f995
Júlio Hoffimann <julio.hoffimann@gmail.com>
2022-06-22 14:17:23 -0700

    Refactor file structure

--- src/PlatformAware.jl
+++ src/PlatformAware.jl
@@ -4,9 +4,23 @@
 
 module PlatformAware
 
-include("quantifiers/quantifiers.jl")
-include("platformtypes/platformtypes.jl")
-include("platform.jl")
+# quantifiers
+include("quantifiers/atleast.jl")
+include("quantifiers/atmost.jl")
+include("quantifiers/macros.jl")
+
+# platform types
+include("platforms/general.jl")
+include("platforms/isa.jl")
+include("platforms/ec2.jl")
+include("platforms/gcp.jl")
+include("platforms/nvidia.jl")
+include("platforms/intel.jl")
+include("platforms/amd.jl")
+include("platforms/xilinx.jl")
+
+# platform identification
+include("identification.jl")
 
 export 
     @platform,
--- src/quantifiers/quantifiers.jl
+++
@@ -1,7 +0,0 @@
-# ------------------------------------------------------------------
-# Licensed under the MIT License. See LICENCE in the project root.
-# ------------------------------------------------------------------
-
-include("quantifiers_atleast.jl")
-include("quantifiers_atmost.jl")
-include("quantifiers_sugar.jl")
