1b89e583653cc0fba4b76dff81a4af3f2f3996b6
pedrosfa <sfa.pedro@gmail.com>
2022-12-14 20:34:00 +0000

    Feature detection

--- src/features/qualifiers/gcp/gcp.jl
+++ src/features/qualifiers/gcp/gcp.jl
@@ -16,6 +16,25 @@ abstract type GoogleCloud <: CloudProvider end; export GoogleCloud
 
 # machine size types
 
-function getNodeFeatures(provider::Type{<:GoogleCloud}, node_features)
-    nothing
+function getMachineType(provider::Type{<:GoogleCloud})
+
+	machine_type_url = "http://metadata.google.internal/computeMetadata/v1/instance/machine-type"
+	machine_type = 
+		try
+			return String(HTTP.request("GET", machine_type_url, ["Metadata-Flavor" => "Google"]))
+		catch e
+			return nothing
+		end
 end
+
+function getDiskInfo(provider::Type{<:GoogleCloud})
+	disk_info_url = "http://metadata.google.internal/computeMetadata/v1/instance/disks/?recursive=true"
+	disk_info = 
+		try
+			return String(HTTP.request("GET", disk_info_url, ["Metadata-Flavor" => "Google"]))
+		catch e
+			return nothing
+		end
+end
+
+
